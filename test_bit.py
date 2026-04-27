fb5c927f65911b4fdcfc85a3226fb549024146d9
kevin046 <linkevin046@gmail.com>
2026-04-27 19:05:33 -0400

    cli: add --help to main entry, improve help output

--- test_bit.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-qh = np.frombuffer(b'\x00' * 64, dtype=np.uint8)
-qh_expanded = np.column_stack([qh] * 4)
-shifts = np.array([6, 4, 2, 0], dtype=np.uint32)
-qh_high = ((qh_expanded >> shifts) & 3).astype(np.float32).ravel()
-print('shape:', qh_high.shape)
